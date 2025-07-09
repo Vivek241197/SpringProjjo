29fdeb22f013d847638e1a5de57315ed993df5aa
Vivek241197 <kvivak952@gmail.com>
2025-07-09 07:41:24 +0530

    Hello

---
+++ ShippingMicroService/src/main/java/com/example/ShippingService/ShippingMicroService/ShippingMicroServiceApplication.java
@@ -0,0 +1,18 @@
+package com.example.ShippingService.ShippingMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingMicroServiceApplication.class, args);
+	} 
+
+}
+
+
+
+
+//***REMOVED***
